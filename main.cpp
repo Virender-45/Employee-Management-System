75053b71341cff1cda0575e63bdbc73f47f84359
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-08 04:17:37 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,6 +23,34 @@ public:
 	void countEmployees();
 	void updateSalary(float newSalary);
 };
+void Employee::addEmployee() {
+	int n;
+	cout << "How many Employees you want to add : ";
+	cin >> n;
+
+	for (int i = 0; i < n; i++){
+		cout << "Enter the first name of the new employee : ";
+		cin >> empHave[i].fname;
+		cout << "Enter the last name of the new employee : ";
+		cin >> empHave[i].lname;
+		cout << "Enter department of the new employee : ";
+		cin >> empHave[i].department;
+		cout << "Enter the salary of the new employee : ";
+		cin >> empHave[i].salary;
+		totalEmp += 1;
+		empHave[i].empId = totalEmp;
+		cout << "Employee with name '" << empHave[i].fname << " " << empHave[i].lname << "' of '" << empHave[i].department << "' department with '" << empHave[i].salary << "' salary is added and gets '" << empHave[i].empId << "' id." << endl;
+	}
+}
+void Employee::displayMenu() {
+	cout << "--------------Welcome--------------" << endl;
+	cout << "\nSelect an option" << endl;
+	cout << "(1) Add a new employee" << endl;
+	cout << "(2) Display details of all employees" << endl;
+	cout << "(3) Search and update an employee's salary by empID" << endl;
+	cout << "(4) Display the total number of employees" << endl;
+	cout << "(5) Exit the program" << endl;
+}
 Employee::Employee() {
 	empHave[0] = { 1, "Virender", "Singh", "Developer", 1000 };
 	empHave[1] = { 2, "Aman", "Dhiman", "Developer", 10000 };
@@ -33,15 +61,6 @@ Employee::Employee() {
 	empHave[6] = { 7, "Nikhil", "Thakur", "SEO", 20000 };
 	empHave[7] = { 8, "Atul", "Rana", "SEO", 30000 };
 	totalEmp = 8;
-}
-void Employee::displayMenu() {
-	cout << "--------------Welcome--------------" << endl;
-	cout << "\nSelect an option" << endl;
-	cout << "(1) Add a new employee" << endl;
-	cout << "(2) Display details of all employees" << endl;
-	cout << "(3) Search and update an employee's salary by empID" << endl;
-	cout << "(4) Display the total number of employees" << endl;
-	cout << "(5) Exit the program" << endl;
 }
 
 int main() {
@@ -54,8 +73,11 @@ int main() {
 		cin >> choice;
 		switch (choice)
 		{
-
+		case 1:
+			wowz.addEmployee();
+			break;
 		default:
+			cout << "Invaild input!!" << endl;
 			break;
 		}
 	} while (choice != 5);
