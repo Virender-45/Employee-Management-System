6ef43634d935471d9c0d3a3e13f9a96bfbcc1058
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-01-08 05:14:04 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,12 @@ using namespace std;
 
 class Employee {
 public:
-	struct emp
-	{
-	int empId;
-	string fname;
-	string lname;
-	string department;
-	float salary;
+	struct emp{
+		int empId;
+		string fname;
+		string lname;
+		string role;
+		float salary;
 	};
 	emp empHave[100];
 	int totalEmp;
@@ -21,61 +20,111 @@ public:
 	void addEmployee();
 	void displayDetails();
 	void countEmployees();
-	void updateSalary(float newSalary);
+	void searchEmployee();
+	void updateSalary();
 };
+void Employee::updateSalary() {
+	for (int i = 0; i < totalEmp; i++){
+		cout << "Name : " << empHave[i].fname << " , ID : " << empHave[i].empId << endl;
+	}
+	int id;
+	cout << "\nEnter the employee ID : ";
+	cin >> id;
+	cout<<"You select "<<
+}
+void Employee::searchEmployee() {
+	int id;
+	cout << "Enter the ID of the employee : ";
+	cin >> id;
+
+	for (int i = 0; i < totalEmp; i++){
+		if (id == empHave[i].empId) {
+			cout << "Employee id : " << id << endl;
+			cout << "Employee Name : " << empHave[i].fname << " " << empHave[i].lname << endl;
+			cout << "Employee Role : " << empHave[i].role << endl;
+			cout << "Employee Salary : " << empHave[i].salary << endl;
+		}
+	}
+}
+void Employee::countEmployees() {
+	cout << "\n\nWe have total '" << totalEmp << "' employees.\n\n";
+}
+void Employee::displayDetails() {
+	for (int i = 0; i < totalEmp; i++){
+		cout << "\nEmployee " << i + 1 << endl;
+		cout << "ID : " << empHave[i].empId << endl;
+		cout << "Name : " << empHave[i].fname << " " << empHave[i].lname << endl;
+		cout << "Role : " << empHave[i].role << endl;
+		cout << "Salary : " << empHave[i].salary << endl;
+	}
+}
 void Employee::addEmployee() {
 	int n;
 	cout << "How many Employees you want to add : ";
 	cin >> n;
 
 	for (int i = 0; i < n; i++){
-		cout << "Enter the first name of the new employee : ";
+		cout << "Enter first name : ";
 		cin >> empHave[i].fname;
-		cout << "Enter the last name of the new employee : ";
+		cout << "Enter the last name : ";
 		cin >> empHave[i].lname;
-		cout << "Enter department of the new employee : ";
-		cin >> empHave[i].department;
-		cout << "Enter the salary of the new employee : ";
+		cout << "Enter role : ";
+		cin >> empHave[i].role;
+		cout << "Enter salary : ";
 		cin >> empHave[i].salary;
 		totalEmp += 1;
-		empHave[i].empId = totalEmp;
-		cout << "Employee with name '" << empHave[i].fname << " " << empHave[i].lname << "' of '" << empHave[i].department << "' department with '" << empHave[i].salary << "' salary is added and gets '" << empHave[i].empId << "' id." << endl;
+		empHave[i].empId = totalEmp + 100;
+		cout << "Employee with name '" << empHave[i].fname << " " << empHave[i].lname << "' added and gets '" << empHave[i].empId << "' id." << endl;
 	}
 }
+Employee::Employee() {
+	empHave[0] = { 101, "Virender", "Singh", "Developer", 1000 };
+	empHave[1] = { 102, "Aman", "Dhiman", "Developer", 10000 };
+	empHave[2] = { 103, "Abhay", "Chandel", "Developer", 100000 };
+	empHave[3] = { 104, "Sahil", "Yadav", "Developer", 1000000 };
+	empHave[4] = { 105, "Pankaj", "Guleria", "Designer", 1000 };
+	empHave[5] = { 106, "Shubham", "Verma", "Designer", 10000 };
+	empHave[6] = { 107, "Nikhil", "Thakur", "SEO", 20000 };
+	empHave[7] = { 108, "Atul", "Rana", "SEO", 30000 };
+	totalEmp = 8;
+}
+
 void Employee::displayMenu() {
 	cout << "--------------Welcome--------------" << endl;
 	cout << "\nSelect an option" << endl;
 	cout << "(1) Add a new employee" << endl;
 	cout << "(2) Display details of all employees" << endl;
-	cout << "(3) Search and update an employee's salary by empID" << endl;
-	cout << "(4) Display the total number of employees" << endl;
-	cout << "(5) Exit the program" << endl;
+	cout << "(3) Display the total number of employees" << endl;
+	cout << "(4) Search an employee's salary by empID" << endl;
+	cout << "(5) Update an employee salary " << endl;
+	cout << "(6) Exit the program" << endl;
+	cout << "Enter your choice : ";
 }
-Employee::Employee() {
-	empHave[0] = { 1, "Virender", "Singh", "Developer", 1000 };
-	empHave[1] = { 2, "Aman", "Dhiman", "Developer", 10000 };
-	empHave[2] = { 3, "Abhay", "Chandel", "Developer", 100000 };
-	empHave[3] = { 4, "Sahil", "Yadav", "Developer", 1000000 };
-	empHave[4] = { 5, "Pankaj", "Guleria", "Designer", 1000 };
-	empHave[5] = { 6, "Shubham", "Verma", "Designer", 10000 };
-	empHave[6] = { 7, "Nikhil", "Thakur", "SEO", 20000 };
-	empHave[7] = { 8, "Atul", "Rana", "SEO", 30000 };
-	totalEmp = 8;
-}
-
 int main() {
 
 	int choice;
 	Employee wowz;
-	do
-	{
+	do{
 		wowz.displayMenu();
 		cin >> choice;
-		switch (choice)
-		{
+		switch (choice){
 		case 1:
 			wowz.addEmployee();
 			break;
+		case 2:
+			wowz.displayDetails();
+			break;
+		case 3:
+			wowz.countEmployees();
+			break;
+		case 4:
+			wowz.searchEmployee();
+			break;
+		case 5:
+			wowz.updateSalary();
+			break;
+		case 6:
+			cout << "Exiting the program..." << endl;
 		default:
 			cout << "Invaild input!!" << endl;
 			break;
